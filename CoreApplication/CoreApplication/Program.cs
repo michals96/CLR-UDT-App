373e98d4ba2bbe24b674730249e55a53bc358a73
michals96 <37269920+michals96@users.noreply.github.com>
2020-06-09 23:53:27 +0200

    UDT First commit. Add project template

---
+++ CoreApplication/CoreApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CoreApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nHello world!\n");
+            Console.WriteLine("\npress any key to exit the process...");
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ Queries.sql
@@ -0,0 +1,16 @@
+-- tutaj tworze baze w ktorej beda sie znajdywac moje UDT
+create database projectdb
+
+-- zalaczam moja baze
+use projectdb
+
+--Utworzenie potrzebnych tabel, przy u¿yciu zdefiniowanych typów danych
+--create table Rownie( rownia dbo.RowniaPochyla);
+--create table Prostokaty ( figura dbo.Prostokat);
+--create table Numbers (ComplexField dbo.ComplexNumber);
+--create table Konta (bank dbo.Konto);
+--create table Kolory (rgb dbo.RGB);
+--create table Wektory (wek dbo.Wektor);
+
+
+
