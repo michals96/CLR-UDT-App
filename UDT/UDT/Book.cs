6fbca68a705259620bd1084c1434ff4976b3c12e
michals96 <37269920+michals96@users.noreply.github.com>
2020-06-11 13:23:07 +0200

    CLR Book type template. Raw version

---
+++ UDT/UDT/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+[Serializable]
+[Microsoft.SqlServer.Server.SqlUserDefinedType(Format.Native)]
+public struct Book : INullable
+{
+    public string author;
+    public string title;
+    public int yearOfPublication;
+    public override string ToString()
+    {
+        // Replace the following code with your code
+        return "";
+    }
+
+    public bool IsNull
+    {
+        get
+        {
+            return m_Null;
+        }
+    }
+
+    public static Book Null
+    {
+        get
+        {
+            Book h = new Book();
+            h.m_Null = true;
+            return h;
+        }
+    }
+
+    public static Book Parse(SqlString s)
+    {
+        if (s.IsNull)
+            return Null;
+        Book u = new Book();
+        // Put your code here
+        return u;
+    }
+
+    // This is a place-holder field member
+    public int var1;
+    // Private member
+    private bool m_Null;
+}
+
+
