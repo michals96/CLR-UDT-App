fc9d207316829c35dbf4ddbf41aff1b5a155c57d
michals96 <37269920+michals96@users.noreply.github.com>
2020-06-11 16:12:49 +0200

    Raw switch statement

--- CoreApplication/CoreApplication/Program.cs
+++ CoreApplication/CoreApplication/Program.cs
@@ -18,6 +18,17 @@ namespace CoreApplication
             Console.Write("1. Rectangle\n");
             Console.Write("2. Exit\n");
             string option = Console.ReadLine();
+            switch (int.Parse(option))
+            {
+                case 1:
+                    Console.WriteLine("RECTANGLE\n");
+                    break;
+                case 2:
+                    Console.WriteLine("EXIT\n");
+                    break;
+                default:
+                    break;
+            }
             Console.ReadKey(); 
         }
     }
