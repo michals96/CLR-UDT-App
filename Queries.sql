9be70a281f0dc10156a6221a80b1ddce4dde19b8
michals96 <37269920+michals96@users.noreply.github.com>
2020-06-11 19:40:00 +0200

    UDT Square complete. First version

--- Queries.sql
+++ Queries.sql
@@ -4,8 +4,15 @@
 -- zalaczam moja baze
 use projectdb
 
+-- RECTANGLES --
 --Utworzenie potrzebnych tabel, przy u¿yciu zdefiniowanych typów danych
-create table Rectangles ( shape dbo.Rectangle);
+--create table Rectangles ( shape dbo.Rectangle);
+
+-- Usuwanie tabeli 
+--drop table Rectangles
+
+-- SQUARE --
+create table Squares ( shape dbo.Square)
 
 
 
